07e1fa2e48bcfa699f6e7a37be1ae5f5757f206f
Sergey <rogovoyserg2006@gmail.com>
2023-06-26 22:26:50 +0300

    check

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+/**
+1)Створюємо  візуал:
+-Привітальне вікно і кнопка «Start» - готово
+-Фон
+-Персонажів які є, потім їх по ходу будемо ховати
+-Табло скільки качом ми вбили
+-Табло рахунок
+-Табло Який раунд
+-Табло пострілів
+*/
+
+btnStartGame.onclick = function() {
+    let startGameBlock = document.querySelector('.start-game');
+    startGameBlock.style.display = 'none';
+    duckHuntIntroSound();
+}
+
 let a = 1;
