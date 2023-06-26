71cf47e7fde3255f8b5d1bf425d619fcf37dc6b9
MarynaRusak <137725390+MarynaRusak@users.noreply.github.com>
2023-06-27 01:02:38 +0300

    Updated script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-
+let btnStartGame = document.getElementById('btnStartGame');
 btnStartGame.onclick = function() {
     let startGameBlock = document.querySelector('.start-game');
     startGameBlock.style.display = 'none';
     duckHuntIntroSound();
+    createDuck();
+    createDog();
+}
+isGameOver = false;
+let app = document.querySelector('#game-elements');
+function random(min,max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
 }
